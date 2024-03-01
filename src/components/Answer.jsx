8c3375f2829e2d897be409432e5796afe12c7c9a
Mykhailo <98221429+mikerozheliuk@users.noreply.github.com>
2024-03-01 17:59:44 +0200

    create quiz (#1)

---
+++ src/components/Answer.jsx
@@ -0,0 +1,10 @@
+const Answer = () => {
+  return (
+    <div className="answer">
+      <div className="answer_letter">A</div>
+      <div className="answer_text">Text of the answer</div>
+    </div>
+  );
+};
+
+export default Answer;
