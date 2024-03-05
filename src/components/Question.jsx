21b12c1825410f627f1f7677c8c92a050a3663ea
Mykhailo <98221429+mikerozheliuk@users.noreply.github.com>
2024-03-05 14:41:54 +0200

    add createContext and useReducer (#2)

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,9 +1,18 @@
+import { useContext } from "react";
+
+import { QuizContext } from "../context/quiz";
+
 import Answer from "./Answer";
 
 const Question = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+  console.log("Question", quizState);
+
+  const currentQuestion = quizState.questions[quizState.currentQuestionIndex];
+
   return (
     <div>
-      <div className="question">Text of the question </div>
+      <div className="question">{currentQuestion.question}</div>
       <div className="answers">
         <Answer />
         <Answer />
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Quiz from "./components/Quiz";
 import "./index.scss";
+import { QuizProvider } from "./context/quiz";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Quiz />
+    <QuizProvider>
+      <Quiz />
+    </QuizProvider>
   </React.StrictMode>
 );
