5abbec3f7a0b01340edf15acaf0c4496ebe521bc
mike <rozheluikmisha@gmail.com>
2024-03-05 14:41:26 +0200

    add createContext and useReducer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,24 @@
+import { useContext } from "react";
+
+import { QuizContext } from "../context/quiz";
+
 import Question from "./Question";
 
 const Quiz = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
     <div className="quiz">
       <div>
-        <div className="score">Question 1/8 </div>
+        <div className="score">
+          Question {quizState.currentQuestionIndex + 1}/{quizState.questions.length}{" "}
+        </div>
+
         <Question />
-        <div className="next_button">Next question</div>
+
+        <div className="next_button" onClick={() => dispatch({ type: "NEXT_QUESTION" })}>
+          Next question
+        </div>
       </div>
     </div>
   );
